988c4a4f08ab63e6399ea7cd5b4d8076a03c3ce0
evnag <vskonev22@gmail.com>
2022-09-20 15:47:07 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/skyprospringexceptions/SkyproSpringExceptionsApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.skyprospringexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringExceptionsApplication.class, args);
+	}
+
+}
